0db6d52c07b53225d2b55c9c5e425a2a2fa39210
loseardes77 <loseardes77@gmail.com>
2024-08-28 19:53:59 +0200

    fix install file

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@
 USER_NAME=$(whoami)
 sed -i "s/USER_NAME/$USER_NAME/g" ./headsetcontrol-notifyd.service
 
+cargo install --path .
+
 sudo cp ./headsetcontrol-notifyd.service /etc/systemd/user/headsetcontrol-notifyd.service
 
-sudo systemctl --user daemon-reload
-sudo systemctl --user enable --now headsetcontrol-notifyd.service
+systemctl --user daemon-reload
+systemctl --user enable --now headsetcontrol-notifyd.service
